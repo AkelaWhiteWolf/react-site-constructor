4600e79af952d030e9e105ca4a0924f279d6d1ff
Alexey Shvets <alexey199822la@gmail.com>
2023-09-14 16:24:53 +0300

    dev/adding blocks added

--- src/components/BlockToChoose.tsx
+++ src/components/BlockToChoose.tsx
@@ -4,6 +4,7 @@ import { Box, Text, VStack } from '@chakra-ui/react';
 import { getTextWithCapitalLetter } from 'src/utils';
 
 import { PossibleBlockType } from 'src/types';
+import { useConstructorsBlocks } from 'src/hooks';
 
 interface IProps {
   type: PossibleBlockType;
@@ -12,9 +13,12 @@ interface IProps {
 
 export const BlockToChoose: React.FC<IProps> = ({ type, children }) => {
   const typeText = useMemo(() => getTextWithCapitalLetter(type), [type]);
+  const { addBlock } = useConstructorsBlocks();
+
+  const handleAddBlock = () => addBlock({ type, content: '' });
 
   return (
-    <Box bg="lightblue.light" padding="15px 10px">
+    <Box bg="lightblue.light" padding="15px 10px" onClick={handleAddBlock}>
       <VStack>
         {children}
         <Text>{typeText}</Text>
